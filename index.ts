f304249c458e79349e43703d58cf6e080e4df26b
CFCircle <iemmessien@proton.me>
2026-06-30 01:11:49 +0100

    initialized backend

--- index.ts
+++ index.ts
@@ -1,17 +1,14 @@
-import express from 'express'
+import { Hono } from 'hono'
 
-const app = express()
+const app = new Hono()
 
-app.get('/', (_req, res) => {
-  res.send('Hello Express!')
+const welcomeStrings = [
+  "Hello Hono!",
+  "To learn more about Hono on Vercel, visit https://vercel.com/docs/frameworks/backend/hono",
+]
+
+app.get('/', (c) => {
+  return c.text(welcomeStrings.join('\n\n'))
 })
 
-app.get('/api/users/:id', (_req, res) => {
-  res.json({ id: _req.params.id })
-})
-
-app.get('/api/posts/:postId/comments/:commentId', (_req, res) => {
-  res.json({ postId: _req.params.postId, commentId: _req.params.commentId })
-})
-
-export default app
+export default app;
